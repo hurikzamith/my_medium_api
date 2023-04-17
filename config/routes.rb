25c2b975539fb6361c76f93a6719934e0c2fb367
hurik zamith <hurikzamith@gmail.com>
2023-04-16 21:21:25 -0300

    csv gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-    namespace 'api' do
-      namespace 'v1' do
-          get 'read_csv'
-        end
+  namespace 'api' do
+    namespace 'v1' do
+      resources :movies, only: [ :index ] do
+        get 'read_csv', on: :collection
       end
+    end
   end
+end
